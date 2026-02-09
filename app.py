3a1b1500ca56e938d992137c10c7e82808dea2df
moosez690 <23248@burnside.school.nz>
2026-02-09 14:16:14 +1300

    made it run

--- app.py
+++ app.py
@@ -4,5 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>" \
-    "<b>e</b>"
+    #home page
+    return "<p>Hello, World!</p>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
